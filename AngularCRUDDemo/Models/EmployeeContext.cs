cbac5207277bb030cefcb59c8e4b12feb0039bcf
hemchanrona <50692232+hemchanrona@users.noreply.github.com>
2020-03-11 20:39:13 +0700

    Add project files.

---
+++ AngularCRUDDemo/Models/EmployeeContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngularCRUDDemo.Models;
+
+namespace AngularCRUDDemo.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext()
+        {
+        }
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AngularCRUDDemo.Models.Employees> Employees { get; set; }
+    }
+}
